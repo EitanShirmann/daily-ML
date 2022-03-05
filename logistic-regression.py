f47a525ec3ff72458902cc7ceeadcb7d5f961e5c
Eitan Shirman <eitanzubu@gmail.com>
2022-03-05 23:52:51 +0200

    inital commit

--- logistic-regression.py
+++ logistic-regression.py
@@ -6,16 +6,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-costs = []
+
 set = datasets.load_breast_cancer()
 X, y = set.data, set.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def cost(A, Y):
+
+    #binary cross-entropy loss
     return np.sum(((- np.log(A))*Y + (-np.log(1-A))*(1-Y)))/len(A)
 
 
@@ -24,29 +25,32 @@ class LogisticRegessor:
         self.lr = lr
         self.n_iters = n_iters
 
+        self.costs = []
         self.weights = None
         self.bias = None
 
 
     def fit(self, X, y):
         
-        # initialize weights 
+        # initialize the weights 
         m, n = X.shape
         self.weights = np.zeros(n)
         self.bias = 0
         
 
-        # gd
+        # gradient descent 
         for _ in range(self.n_iters):
-            yhat = np.dot(X, self.weights)+self.bias
-            yhat = sigmoid(yhat)
+            yhat = sigmoid(np.dot(X, self.weights)+self.bias)
+            
             dw   = np.dot(X.T, (yhat - y))/m
             db   = np.sum(yhat-y)/m
+
             if _%10==0:
-                costs.append(cost(yhat, y))
+                self.costs.append(cost(yhat, y))
+
             self.weights -= self.lr * dw
             self.bias -= self.lr * db
-        plt.plot(costs)
+        plt.plot(self.costs)
         plt.title("Costs")
         plt.show()
         
@@ -54,8 +58,8 @@ class LogisticRegessor:
 
     
     def predict(self, X):
+
         preds = sigmoid(np.dot(X, self.weights)+self.bias)
-        
         return [1 if i>0.5 else 0 for i in preds]
         
 
@@ -69,4 +73,4 @@ regressor = LogisticRegessor(lr=0.00001, n_iters=10000)
 regressor.fit(X_train, y_train)
 predictions = regressor.predict(X_test)
 
-print("LogisticRegessor classification accuracy:", accuracy(y_test, predictions))
+print("Logistic Regessor classification accuracy:", accuracy(y_test, predictions))
---
+++ readme.md
@@ -0,0 +1,21 @@
+# This is a daily implementation of a popular ML algorithm.
+
+
+<ol>
+<li> KNN classifier</li>
+<br>
+<p>Classify a datapoint by label voting among the closest points from the training set.</p>
+
+<li> Linear regression</li>
+<br>
+<p>Find a line that best fits the countinuous-value data, by minizing MSE between training points and the line. </p>
+
+<li> Logistic regression</li>
+<br>
+<p>Linear, binary classifier trained on breast cancer dataset</p>
+
+
+<li> Support vector machine</li>
+<br>
+<p>Finding a linear hyperplane, which acts as the optimal decision boundary for separating classes with biggest margin ||w||.</p>
+
